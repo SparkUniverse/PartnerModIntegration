010d53f2f5378f421eab8ed864277d5c6a76920a
sychic <47618543+Sychic@users.noreply.github.com>
2026-02-16 11:08:31 -0500

    Bump version to 1.0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "gg.essential"
-version = "1.0.5"
+version = "1.0.6"
 
 repositories {
     maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1")
